1db668098f42b6d3182c3a9e504ffa5a40857ea9
guojj <m15118293606@163.com>
2022-09-06 13:57:33 +0800

    test merge hotfix

--- src/test.java
+++ src/test.java
@@ -2,5 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("222");
+        System.out.println("master");
+        System.out.println("hotfix");
     }
 }
