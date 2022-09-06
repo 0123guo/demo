fc8f30717e6b0786e76e70498bfb9031b451fbdb
guojj <m15118293606@163.com>
2022-09-06 14:08:12 +0800

    test merge conflict - hotfix

--- src/test.java
+++ src/test.java
@@ -4,5 +4,6 @@ public class test {
         System.out.println("222");
         System.out.println("master");
         System.out.println("hotfix");
+        System.out.println("test merge conflict hot-fix");
     }
 }
